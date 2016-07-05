efbbfa7f10d4e537ead0ac84c3d5621aed812253
qq1156092409 <1156092409@qq.com>
2016-07-06 00:21:25 +0800

    gzh

--- controllers/GzhController.php
+++ controllers/GzhController.php
@@ -2,102 +2,28 @@
 
 namespace app\controllers;
 use app\models\form\GzhForm;
-use app\models\Test2;
-use yii\helpers\FileHelper;
 use yii\web\Controller;
 
 class GzhController extends Controller{
+    public $enableCsrfValidation=false;
     public function actionIndex(){
-        //监听请求
-        $test2=new Test2();
-        $test2->name=json_encode(\Yii::$app->request->getQueryParams());
-
-        define("TOKEN", "weixin");
-        $wechatObj = new wechatCallbackapiTest();
-        if(isset($_GET["echostr"])){
-            $test2->save();
-            $wechatObj->valid();
-        }else{
-            $test2->name="haha";
-            $test2->save();
-            $wechatObj->responseMsg();
-        }
-    }
-}
-class wechatCallbackapiTest
-{
-    public function valid()
-    {
-        $echoStr = $_GET["echostr"];
-
-        //valid signature , option
-        if($this->checkSignature()){
-            echo $echoStr;
-            exit;
-        }
-    }
-
-    public function responseMsg()
-    {
-        //get post data, May be due to the different environments
-        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
-
-        //extract post data
-        if (!empty($postStr)){
-            /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
-               the best way is to check the validity of xml by yourself */
-            libxml_disable_entity_loader(true);
-            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-            $fromUsername = $postObj->FromUserName;
-            $toUsername = $postObj->ToUserName;
-            $keyword = trim($postObj->Content);
-            $time = time();
-            $textTpl = "<xml>
-							<ToUserName><![CDATA[%s]]></ToUserName>
-							<FromUserName><![CDATA[%s]]></FromUserName>
-							<CreateTime>%s</CreateTime>
-							<MsgType><![CDATA[%s]]></MsgType>
-							<Content><![CDATA[%s]]></Content>
-							<FuncFlag>0</FuncFlag>
-							</xml>";
-            if(!empty( $keyword ))
-            {
-                $msgType = "text";
-                $contentStr = "Welcome to wechat world!";
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                echo $resultStr;
+        $model=new GzhForm();
+        if(\Yii::$app->request->isPost){
+            $model->scenario=GzhForm::ANSWER;
+            $model->load(\Yii::$app->request->post(),"");
+            if($model->answer()){
+                echo $model->getRet();
             }else{
-                echo "Input something...";
+                foreach($model->errors as $k=>$v){
+                    echo $v[0];break;
+                }
             }
-
-        }else {
-            echo "";
-            exit;
+            \Yii::$app->end();
         }
-    }
-
-    private function checkSignature()
-    {
-        // you must define TOKEN by yourself
-        if (!defined("TOKEN")) {
-            throw new \Exception('TOKEN is not defined!');
-        }
-
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];
-
-        $token = TOKEN;
-        $tmpArr = array($token, $timestamp, $nonce);
-        // use SORT_STRING rule
-        sort($tmpArr, SORT_STRING);
-        $tmpStr = implode( $tmpArr );
-        $tmpStr = sha1( $tmpStr );
-
-        if( $tmpStr == $signature ){
-            return true;
-        }else{
-            return false;
+        $model->scenario=GzhForm::VALID;
+        $model->load(\Yii::$app->request->getQueryParams(),"");
+        if($model->valid()){
+            echo $model->getRet();
         }
     }
 }
--- models/form/GzhForm.php
+++ models/form/GzhForm.php
@@ -1,26 +1,74 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 2016/7/5
- * Time: 20:48
- */
 
 namespace app\models\form;
-
-
 use yii\base\Model;
 
 class GzhForm extends Model {
+    const VALID = "valid";
+    const ANSWER = "answer";
+    const TOKEN="weixin";
+
     public $signature;
     public $timestamp;
     public $nonce;
     public $echostr;
 
-    private $_token="weixin";
+    public $FromUserName;
+    public $ToUserName;
+    public $Content;
+
+    private $_ret;
+
+    public function scenarios(){
+        return [
+            self::VALID=>["signature","timestamp","nonce","echostr"],
+            self::ANSWER=>["FromUserName","ToUserName","Content"],
+        ];
+    }
+    public function rules(){
+        return [
+            ["signature","checkSignature"],
+        ];
+    }
+    public function checkSignature($attr){
+        $tmpArr = array(self::TOKEN, $this->timestamp, $this->nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr != $this->signature ){
+            $this->addError($attr, "校验失败");
+        }
+    }
 
     public function valid(){
-
+        if($flag=$this->validate()){
+            $this->_ret=$this->echostr;
+        }
+        return $flag;
     }
+    public function answer($validate=true){
+        if($validate && !$this->validate()) return false;
+        $this->processAnswer($this->getAnswer());
+        return true;
+    }
+    private function processAnswer($answer){
+        $textTpl = "<xml>
+                        <ToUserName><![CDATA[%s]]></ToUserName>
+                        <FromUserName><![CDATA[%s]]></FromUserName>
+                        <CreateTime>%s</CreateTime>
+                        <MsgType><![CDATA[%s]]></MsgType>
+                        <Content><![CDATA[%s]]></Content>
+                        <FuncFlag>0</FuncFlag>
+                        </xml>";
+        $this->_ret = sprintf($textTpl, $this->FromUserName, $this->ToUserName, time(), "text", $answer);
+    }
+    private function getAnswer(){
+        return "回复:".$this->Content;
+    }
+    public function getRet(){
+        return $this->_ret;
+    }
+
 
 }
